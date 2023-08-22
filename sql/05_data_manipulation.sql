ffc8ff792a5db50665a3369453598ade4abe1a5f
jenniii-om <141550650+jenniii-om@users.noreply.github.com>
2023-08-22 23:18:03 +1000

    subquery with scalar and list

--- sql/05_data_manipulation.sql
+++ sql/05_data_manipulation.sql
@@ -129,3 +129,180 @@ WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
 
 
 
+
+-- Filtering CASE statement
+
+-- Let's generate a list of matches won by Italy's Bologna team! There are quite a few additional teams in the two tables, so a key part of generating a usable query will be using your CASE statement as a filter in the WHERE clause.
+
+-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+-- Filter for team long name
+WHERE team_long_name = 'Bologna';
+
+
+-- Select the season and date columns
+SELECT 
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal 
+            THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal 
+            THEN 'Bologna Win' 
+		END AS outcome
+FROM matches_italy;
+
+
+-- Select the season, date, home_goal, and away_goal columns
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
+
+
+
+
+
+-- CASE with aggregate functions
+
+-- Do the number of soccer matches played in a given European country differ across seasons? We will use the European Soccer Database to answer this question.
+
+-- You will examine the number of matches played in 3 seasons within each country listed in the database. This is much easier to explore with each season's matches in separate columns. Using the country and unfiltered match table, you will count the number of matches played in each country during the 2012/2013, 2013/2014, and 2014/2015 match seasons.
+
+SELECT 
+	c.name AS country,
+    -- Count games from the 2012/2013 season
+	COUNT(CASE WHEN m.season = '2012/2013' 
+        	THEN m.id ELSE NULL END) AS matches_2012_2013
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+SELECT 
+	c.name AS country,
+    -- Count matches in each of the 3 seasons
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+
+
+-- SUM / CASE WHEN with multiple conditions
+
+-- In R or Python, you have the ability to calculate a SUM of logical values (i.e., TRUE/FALSE) directly. In SQL, you have to convert these values into 1 and 0 before calculating a sum. This can be done using a CASE statement.
+-- There's one key difference when using SUM to aggregate logical values compared to using COUNT in the previous exercise --
+-- Your goal here is to use the country and match table to determine the total number of matches won by the home team in each country during the 2012/2013, 2013/2014, and 2014/2015 seasons.
+
+SELECT 
+	c.name AS country,
+    -- Sum the total records in each season where the home team won
+	SUM(CASE WHEN m.season = '2012/2013' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2012_2013,
+ 	SUM(CASE WHEN m.season = '2013/2014' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2013_2014,
+	SUM(CASE WHEN m.season = '2014/2015' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+-- CASE and AVG
+-- Your task is to examine the number of wins, losses, and ties in each country. The matches table is filtered to include all matches from the 2013/2014 and 2014/2015 seasons.
+
+SELECT 
+    c.name AS country,
+    -- Count the home wins, away wins, and ties in each country
+	COUNT(CASE WHEN m.home_goal > m.away_goal THEN m.id 
+        END) AS home_wins,
+	COUNT(CASE WHEN m.home_goal < m.away_goal THEN m.id 
+        END) AS away_wins,
+	COUNT(CASE WHEN m.home_goal = m.away_goal THEN m.id 
+        END) AS ties
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+SELECT 
+	c.name AS country,
+    -- Round the percentage of tied games to 2 decimal points
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+
+-- SUBQUERY
+
+-- Scalar subqueries
+-- Generate a list of matches where the total goals scored (for both teams in total) is more than 3 times the average for games in the matches_2013_2014 table, which includes all games played in the 2013/2014 season.
+
+
+SELECT 
+	-- Select the date, home goals, and away goals scored
+    date,
+	home_goal,
+	away_goal
+FROM  matches_2013_2014
+-- Filter for matches where total goals exceeds 3x the average
+WHERE (home_goal + away_goal) > 
+       (SELECT 3 * AVG(home_goal + away_goal)
+        FROM matches_2013_2014); 
+
+
+
+-- Subquery with a list
+
+-- Your goal in this exercise is to generate a list of teams that never played a game in their home city. Using a subquery, you will generate a list of unique hometeam_ID values from the unfiltered match table to exclude in the team table's team_api_ID column.
+
+SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team 
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN
+     (SELECT DISTINCT hometeam_id  FROM match);
+
+
+-- Let's do some further exploration in this database by creating a list of teams that scored 8 or more goals in a home match.
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT hometeam_id 
+       FROM match
+       WHERE home_goal >= 8);

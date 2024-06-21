7c06bed70d6d25d3e8febceec3e227d06a7d1f32
jenniii-om <141550650+jenniii-om@users.noreply.github.com>
2024-06-21 11:44:23 +1000

    exercise using INSERT INTO temp table

--- 09_Exploratory_Data_Analysis_in_SQL/02_summarizing_and_aggregating_numeric_data.sql
+++ 09_Exploratory_Data_Analysis_in_SQL/02_summarizing_and_aggregating_numeric_data.sql
@@ -214,3 +214,41 @@ SELECT 'revenues_change'::varchar AS measure
 
 
 -- SELECT * FROM correlations;
+
+
+-- Instructions 3/3
+-- Select all rows and columns from the correlations table to view the correlation matrix.
+-- First, you will need to round each correlation to 2 decimal places.
+-- The output of corr() is of type double precision, so you will need to also cast columns to numeric.
+
+DROP TABLE IF EXISTS correlations;
+
+CREATE TEMP TABLE correlations AS
+SELECT 'profits'::varchar AS measure
+       ,corr(profits, profits) AS profits
+       ,corr(profits, profits_change) AS profits_change
+       ,corr(profits, revenues_change) AS revenues_change
+  FROM fortune500;
+
+INSERT INTO correlations
+SELECT 'profits_change'::varchar AS measure
+       ,corr(profits_change, profits) AS profits
+       ,corr(profits_change, profits_change) AS profits_change
+       ,corr(profits_change, revenues_change) AS revenues_change
+  FROM fortune500;
+
+INSERT INTO correlations
+SELECT 'revenues_change'::varchar AS measure
+       ,corr(revenues_change, profits) AS profits
+       ,corr(revenues_change, profits_change) AS profits_change
+       ,corr(revenues_change, revenues_change) AS revenues_change
+  FROM fortune500;
+
+-- Select each column, rounding the correlations
+SELECT measure, 
+       round(profits::numeric,2) AS profits
+       ,round(profits_change::numeric,2) AS profits_change
+       ,round(revenues_change::numeric,2) AS revenues_change
+  FROM correlations;
+
+  
